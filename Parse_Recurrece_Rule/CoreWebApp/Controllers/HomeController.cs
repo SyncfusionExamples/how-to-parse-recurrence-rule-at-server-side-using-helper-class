4051cd204163b76199a7017098e833d78b9850e7
Praveenkumar Narasimhanaidu <praveenkumar.narasimhanaidu@syncfusion.com>
2022-05-05 23:37:21 +0530

    sample added

---
+++ Parse_Recurrece_Rule/CoreWebApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using CoreWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CoreWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult getDates(string dates)
+        {
+            var recurrenceRule = JsonConvert.DeserializeObject<string>(dates);
+            var dateCollection = RecurrenceHelper.GetRecurrenceDateTimeCollection(recurrenceRule, DateTime.Now);
+            return Json(dateCollection);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
